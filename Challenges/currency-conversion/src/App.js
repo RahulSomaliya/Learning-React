143089aa5c6a632e103a7cf04e4cfc15283abe3f
Rahul Somaliya <rahulsomaliya007@gmail.com>
2023-08-09 16:00:42 +0530

    currency conversion challenge slayed!

--- Challenges/currency-conversion/src/App.js
+++ Challenges/currency-conversion/src/App.js
@@ -1,5 +1,66 @@
-function App() {
-  return <div>currency conversion</div>;
+import { useEffect, useState } from 'react';
+
+//
+
+export default function App() {
+  const [amount, setAmount] = useState('');
+  const [fromCurrency, setFromCurrency] = useState('USD');
+  const [toCurrency, setToCurrency] = useState('INR');
+  const [convertedRate, setConvertedRate] = useState('');
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+
+      async function calculateConversion() {
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+            { signal: controller.signal }
+          );
+
+          const { rates } = await res.json();
+
+          setConvertedRate(rates?.[toCurrency] || amount);
+        } catch (error) {
+          if (error === 'AbortError') {
+            console.log('abort is working!!!');
+          }
+        }
+      }
+
+      calculateConversion();
+      return function () {
+        controller.abort();
+      };
+    },
+    [amount, fromCurrency, toCurrency]
+  );
+
+  return (
+    <div>
+      <input
+        type='number'
+        value={amount}
+        onChange={e => setAmount(Number(e.target.value))}
+      />
+
+      <CurrencySelector value={fromCurrency} updateCurrency={setFromCurrency} />
+
+      <CurrencySelector value={toCurrency} updateCurrency={setToCurrency} />
+
+      <p>{convertedRate}</p>
+    </div>
+  );
 }
 
-export default App;
+function CurrencySelector({ value, updateCurrency }) {
+  return (
+    <select value={value} onChange={e => updateCurrency(e.target.value)}>
+      <option value='USD'>USD</option>
+      <option value='EUR'>EUR</option>
+      <option value='CAD'>CAD</option>
+      <option value='INR'>INR</option>
+    </select>
+  );
+}
