a6d97af48b8484ae191111f766eb13254bdc4890
Rahul Somaliya <rahulsomaliya007@gmail.com>
2023-08-25 11:47:18 +0530

    Skeleton code written for react-router with loader functionality (version 6)

--- fast-react-pizza/src/services/apiRestaurant.js
+++ fast-react-pizza/src/services/apiRestaurant.js
@@ -1,21 +1,29 @@
 const API_URL = 'https://react-fast-pizza-api.onrender.com/api';
 
 export async function getMenu() {
-  const res = await fetch(`${API_URL}/menu`);
+  try {
+    const res = await fetch(`${API_URL}/menu`);
 
-  // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
-  if (!res.ok) throw Error('Failed getting menu');
+    // fetch won't throw error on 400 errors (e.g. when URL is wrong), so we need to do it manually. This will then go into the catch block, where the message is set
+    if (!res.ok) throw Error('Failed getting menu');
 
-  const { data } = await res.json();
-  return data;
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 }
 
 export async function getOrder(id) {
-  const res = await fetch(`${API_URL}/order/${id}`);
-  if (!res.ok) throw Error(`Couldn't find order #${id}`);
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`);
+    if (!res.ok) throw Error(`Couldn't find order #${id}`);
 
-  const { data } = await res.json();
-  return data;
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
 }
 
 export async function createOrder(newOrder) {
