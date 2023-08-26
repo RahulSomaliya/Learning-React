e2c4bc5224eed0317a3156bf0f761d592951c31a
Rahul Somaliya <rahulsomaliya007@gmail.com>
2023-08-26 12:00:54 +0530

    react router features learned - loader, action, errorElement. used useLoaderData to load order page, also used useNavigation() feature to detect the state of routes, and used useRouteError() to display error component

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import CartOverview from '../features/cart/CartOverview';
 import Header from './Header';
+import Loader from './Loader';
 
 function AppLayout() {
+  // this is react-router hook that provides states like loading / idle / submitting / and more
+  // note: this hook will say loading for any of the routes that are mentioned in the createBrowserRouter's array
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div>
+    <div className='layout'>
+      {isLoading && <Loader />}
+
       <Header />
 
       <main>
